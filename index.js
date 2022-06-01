92eab01f26ad9f8bd7902e8bd4b5069ed5f10d29
valentineejk <valentineejk@gmail.com>
2022-06-01 21:12:08 +0100

    re

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-const express = require('express')
 const dotenv = require('dotenv').config()
-const app = express()
-const port = 3000
-const paystack = require('./paystack-charge')
+const https = require('https')
 
+const amount = 30000
 
-app.get('/', (req, res) => {
-    res.send(paystack)
+const secret = process.env.PAYSTACK_SECRET_KEY
+
+const params = JSON.stringify({
+    "email": "customer@email.com",
+    "amount": `${amount}`
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+const options = {
+    hostname: 'api.paystack.co',
+    port: 443,
+    path: '/transaction/initialize',
+    method: 'POST',
+    headers: {
+        Authorization: `Bearer ${secret}`,
+        'Content-Type': 'application/json'
+    }
+}
+const req = https.request(options, res => {
+    let data = ''
+    res.on('data', (chunk) => {
+        data += chunk
+    });
+    res.on('end', () => {
+
+        console.log(JSON.parse(data))
+
+
+    })
+}).on('error', error => {
+    console.error(error)
 })
+
+req.write(params)
+req.end()
